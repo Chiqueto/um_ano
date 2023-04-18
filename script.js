1d9307a06405233be2b846fd33250788e5076aaa
Chiqueto <lfchiqueto@gmail.com>
2023-04-18 08:42:24 -0300

    commit  inicial

---
+++ script.js
@@ -0,0 +1,16 @@
+let imgs =  document.getElementById("img")
+let img = document.querySelectorAll("#img img")
+
+let idx = 0
+
+function carrossel(){
+    idx++
+
+    if(idx > img.length - 1){
+        idx = 0
+    }
+
+    imgs.style.transform = `translateX(${-idx * 400}px)`
+}
+
+setInterval(carrossel, 1800)
