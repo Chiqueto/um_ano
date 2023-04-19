e397d6dfecf314ad6eda4888e42577875286dd3e
Chiqueto <lfchiqueto@gmail.com>
2023-04-19 10:42:52 -0300

    melhorias

--- script.js
+++ script.js
@@ -1,18 +1,70 @@
-let imgs =  document.getElementById("img")
-let img = document.querySelectorAll("#img img")
-let car1 = document.getElementsByClassName("carrossel1")
-let cont1 = document.querySelectorAll(".container1 img")
-let idx = 0
-
-function carrossel(){
-    idx++
-
-    if(idx > img.length - 1){
-        idx = 0
-    }
-    imgs.style.transform = `translateX(${-idx * 450}px)`
-
-    
+const btnInico = document.getElementById("inicio")
+const fotos = document.getElementById("fotos")
+function inicio(){
+    //btnInicio.addEventListener('click' fotos.classList.remove(''))
 }
 
-setInterval(carrossel, 1800)
+
+
+
+class SlideStories{
+    constructor(id){
+        this.slide = document.querySelector(`[data-slide="${id}"]`)
+        this.active = 0
+        this.init()
+    }
+    activeSlide(index){
+        this.active = index
+        this.items.forEach(item => item.classList.remove('active'))
+        this.items[index].classList.add('active')
+        this.thumbItems.forEach(item => item.classList.remove('active'))
+        this.thumbItems[index].classList.add('active')
+        this.autoSlide()
+    }
+
+    prev(){
+        if(this.active > 0){
+            this.activeSlide(this.active - 1)
+        }else{
+            this.activeSlide(this.items.length - 1)
+        }   
+    }
+    next(){
+        if(this.active < this.items.length - 1){
+            this.activeSlide(this.active + 1)
+        }else{
+            this.activeSlide(0)
+        }
+    }
+
+    addThumbItems(){
+        this.items.forEach(() => (this.thumb.innerHTML += '<span></span>'))
+        this.thumbItems = Array.from(this.thumb.children)
+        console.log(this.thumbItems)
+    }
+
+    addNavigation(){
+        const nextBtn = this.slide.querySelector('.slide-next')
+        nextBtn.addEventListener('click', this.next)
+        const prevBtn = this.slide.querySelector('.slide-prev')
+        prevBtn.addEventListener('click', this.prev)
+    }
+
+    autoSlide(){
+        clearTimeout(this.timeout) 
+        this.timeout = setTimeout(this.next, 3000)
+    }
+
+    init(){
+        this.next = this.next.bind(this)
+        this.prev = this.prev.bind(this)
+        this.items =  this.slide.querySelectorAll('.slide-items > *')
+        this.thumb = this.slide.querySelector('.slide-thumb')
+        this.addThumbItems()
+        this.activeSlide(0)
+        this.addNavigation()
+    }
+
+
+}
+new SlideStories('slide')
