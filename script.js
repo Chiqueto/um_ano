e118a0a7761145fa76268432de11cbc0d2867073
Chiqueto <lfchiqueto@gmail.com>
2023-04-24 14:44:44 -0300

    melhorias

--- script.js
+++ script.js
@@ -1,10 +1,45 @@
+const btnsim =  document.getElementById('btnsim')
+const btnnao = document.getElementById('btnnao')
+let res = document.getElementById('res')
+let c = 0
+
+function mudapos(){
+    if(c == 0 || c == 1){
+        btnsim.style.visibility = 'hidden'
+    }else{
+        res.innerHTML = 'Que bom viu, porque vai ter que aguentar pro resto da sua vida, <a href="texto.html">vê aqui</a> o que tenho pra te falar, mas eu também sou muitooooo feliz por estar com você! Obrigado por tudo!'
+        res.innerHTML += '<br>Mas antes de ver o textinho ouve a musica fazendo um favor, porque ela é para você'
+        res.innerHTML += '<audio preload="auto" autoplay> <source src="midia/Jorge & Mateus - Menina Maluquinha.mp3" type="audio/mpeg"> </audio>'
+        
+
+    }
+    
+}
+
+function clicknao(){
+    if (c == 0){
+        res.innerHTML = '<p>Você ta ficando louca!???</p>'
+        res.innerHTML += 'Vou te dar mais uma chance hein gatinha<br> Tenta de novo!'
+        btnsim.style.visibility = 'visible'
+        c++
+    }else if (c == 1){
+        res.innerHTML = '<p>De novo???<br>Eu acho que você ainda não ta maluca né?<br> Vai lá, mais uma </p>'
+        btnsim.style.visibility = 'visible'
+        c++
+    }else{
+        res.innerHTML = 'Agora eu não deixo mais...'
+        btnnao.style.visibility = 'hidden'
+    }
+}
+
+
+
+
 const btnInico = document.getElementById("inicio")
 const fotos = document.getElementById("fotos")
 function inicio(){
     //btnInicio.addEventListener('click' fotos.classList.remove(''))
 }
-
-
 
 
 class SlideStories{
@@ -68,3 +103,7 @@ class SlideStories{
 
 }
 new SlideStories('slide')
+
+
+
+
